e14cac2b00cc421a72a868a5f50008e038e0b05a
Tom Gowan <s3549186@student.rmit.edu.au>
2018-12-27 18:28:16 +1100

    slow fix

--- vulkano/src/sync/future/fence_signal.rs
+++ vulkano/src/sync/future/fence_signal.rs
@@ -164,6 +164,8 @@ impl<F> FenceSignalFuture<F>
 
         match *state {
             FenceSignalFutureState::Flushed(ref mut prev, ref fence) => {
+                /*
+                std::thread::sleep(Duration::from_millis(1));
                 match fence.wait(Some(Duration::from_secs(0))) {
                     Ok(()) => unsafe {
                         prev.signal_finished()
@@ -173,6 +175,21 @@ impl<F> FenceSignalFuture<F>
                         return;
                     },
                 }
+                */
+                std::thread::sleep(Duration::from_millis(1));
+                match fence.ready() {
+                    Ok(true) => unsafe {
+                        prev.signal_finished()
+                    },
+                    Ok(false) => {
+                        prev.cleanup_finished();
+                        return;
+                    },
+                    Err(_) => {
+                        prev.cleanup_finished();
+                        return;
+                    },
+                }               
             },
             FenceSignalFutureState::Pending(ref mut prev, _) => {
                 prev.cleanup_finished();
